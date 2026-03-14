3d98433183d6aab7f471348bd72152617a88154d
Ezequiel Coronel <ezeqiiel50@gmail.com>
2026-03-14 01:46:03 -0300

    Agregar progreso en el endpoint Eliminar producto

---
+++ BE_StockManagementChallenge/StockManagementChallenge/StockManager.Aplication/UsesCases/Product/Delete/ProductDeleteValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace StockManager.Application.UsesCases.Product.Delete
+{
+    public class ProductDeleteValidator : AbstractValidator<ProductDeleteCommand>
+    {
+        public ProductDeleteValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotNull()
+                .GreaterThan(0).WithMessage("{PropertyName} no es valida.");
+        }
+    }
+}
