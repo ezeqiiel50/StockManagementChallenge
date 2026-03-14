fac6b5cfadf2d1f9b984d60fd19cc7a92a8aed19
Ezequiel Coronel <ezeqiiel50@gmail.com>
2026-03-14 02:02:57 -0300

    Agregar progreso en el endpoint Delete product

--- BE_StockManagementChallenge/StockManagementChallenge/StockManager.Api/Controllers/V1/ProductController.cs
+++ BE_StockManagementChallenge/StockManagementChallenge/StockManager.Api/Controllers/V1/ProductController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ROP.APIExtensions;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace StockManagerApi.Controllers.V1
 {
